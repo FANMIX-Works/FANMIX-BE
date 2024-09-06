187e30bd2d58ab8efcb71e82ba7fb2217c58c6fb
zoomin3022 <zoomin3022@gmail.com>
2024-09-06 18:31:06 +0900

    📦 chore: Initial Commit 프로젝트 초기 설정

---
+++ src/main/java/com/example/fanmix/FanmixApplication.java
@@ -0,0 +1,12 @@
+package com.example.fanmix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanmixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanmixApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/fanmix/FanmixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fanmix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanmixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
