f929849120cb0512bd11e1da2fee7b2b53c31b5a
dk <dkfbt1379@gmail.com>
2024-09-09 01:17:37 +0900

    feat : 소셜로그인, 수동회원가입 같이 구현중   서비스전까지 구현완료

---
+++ src/main/java/com/fanmix/api/domain/member/dto/MemberSignUpDto.java
@@ -0,0 +1,26 @@
+package com.fanmix.api.domain.member.dto;
+
+import com.fanmix.api.domain.common.Role;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+/**
+ * 자체로그인 회원가입 API의 RequestBody로 사용
+ */
+public class MemberSignUpDto {
+
+	private String loginId;
+	private String loginPw;
+	private String name;
+	private String profileImgUrl;         //프로필 이미지 경로
+	private String introduce;             //자기소개
+	private String nickName;
+	private String email;
+	private Character gender;            //'M', 'W'
+	private int birthYear;               //탄생년도. 나이는 오늘날짜로부터 계산
+	private String nationality;          //국적
+	private Role role;
+}
