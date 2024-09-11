2b5ea7b71b0cbd56e55bd45678b672b17dd477f9
zoomin3022 <zoomin3022@gmail.com>
2024-09-11 21:44:12 +0900

    ✨ feat:#3 성공 실패 응답의 공통 양식인 Response Record 추가

---
+++ src/main/java/com/fanmix/api/common/response/Response.java
@@ -0,0 +1,24 @@
+package com.fanmix.api.common.response;
+
+public record Response<T>(
+	String status,
+	String customCode,
+	T data,
+	String message
+) {
+	private static final String SUCCESS_STATUS = "SUCCESS";
+	private static final String FAIL_STATUS = "FAIL";
+
+	public static <T> Response<T> success() {
+		return new Response<>(SUCCESS_STATUS, null, null, null);
+	}
+
+	public static <T> Response<T> success(T data) {
+		return new Response<>(SUCCESS_STATUS, null, data, null);
+	}
+
+	public static Response<String> fail(String customCode, String message) {
+		return new Response<>(FAIL_STATUS, customCode, null, message);
+	}
+
+}
