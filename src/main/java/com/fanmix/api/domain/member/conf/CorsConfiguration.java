b781476224d5881d3bf061397d69742385ac04b9
DESKTOP-ggoomter\ggoomter <ggoomter@gmail.com>
2024-09-16 23:14:36 +0900

    fix : CORS설정 재수정

--- src/main/java/com/fanmix/api/domain/member/conf/CorsConfiguration.java
+++ src/main/java/com/fanmix/api/domain/member/conf/CorsConfiguration.java
@@ -1,7 +1,5 @@
 package com.fanmix.api.domain.member.conf;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,11 +11,10 @@ public class CorsConfiguration implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		/* allowedOrigins 을 체이닝으로 쓰면 마지막호출로 덮어씌워지는것에 유의 */
 		registry.addMapping("/**")
-			.allowedOrigins(String.valueOf(
-				List.of("http://localhost:3000", "https://prism-fe.vercel.app", "https://prism.swygbro.com")))
+			.allowedOrigins("http://localhost:3000", "https://prism-fe.vercel.app", "https://prism.swygbro.com")
 			.allowCredentials(true)
-			.allowedHeaders(String.valueOf(List.of("Authorization", "Cache-Control", "Content-Type")))
-			.allowedMethods(String.valueOf(List.of("GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")));
+			.allowedHeaders("Authorization", "Cache-Control", "Content-Type")
+			.allowedMethods("GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
 
 	}
 }
