4594a997e470fd04d8c6174375468dd22a23b4e0
ujubongbong <ujubongbong@gmail.com>
2024-09-18 23:14:04 +0900

    :recycle: refactor: 커뮤니티 추가시 인플루언서 id도 받도록 수정

--- src/main/java/com/fanmix/api/domain/community/dto/AddCommunityRequest.java
+++ src/main/java/com/fanmix/api/domain/community/dto/AddCommunityRequest.java
@@ -1,11 +1,8 @@
 package com.fanmix.api.domain.community.dto;
 
-import com.fanmix.api.domain.common.Role;
 import com.fanmix.api.domain.community.entity.Community;
 
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,12 +10,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 public class AddCommunityRequest {
+	private int influencerId;
 	private String name;
 	private boolean show_yn;
 
 	// 엔티티 변환
 	public Community toEntity() {
 		return Community.builder()
+			.influencerId(influencerId)
 			.name(name)
 			.isShow(show_yn)
 			.build();
