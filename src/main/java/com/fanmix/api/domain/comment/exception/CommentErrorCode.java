9af26761dc06a3b266673034fdc36dd8ef53721c
ujubongbong <ujubongbong@gmail.com>
2024-09-19 10:21:08 +0900

    :sparkles: feat: 댓글 관련 예외 추가

--- src/main/java/com/fanmix/api/domain/comment/exception/CommentErrorCode.java
+++ src/main/java/com/fanmix/api/domain/comment/exception/CommentErrorCode.java
@@ -12,7 +12,8 @@ import lombok.RequiredArgsConstructor;
 public enum CommentErrorCode implements ErrorCode {
 
 	PARENT_ID_NOT_EXIST(HttpStatus.INTERNAL_SERVER_ERROR, "4-001", "상위 댓글이 존재하지 않습니다."),
-	COMMENT_NOT_EXIST(HttpStatus.INTERNAL_SERVER_ERROR, "4-002", "해당 댓글을 찾을 수 없습니다.");
+	COMMENT_NOT_EXIST(HttpStatus.INTERNAL_SERVER_ERROR, "4-002", "해당 댓글을 찾을 수 없습니다."),
+	COMMENT_NOT_BELONG_TO_POST(HttpStatus.INTERNAL_SERVER_ERROR, "4-003", "게시글에서 해당 댓글을 찾을 수 없습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String customCode;
