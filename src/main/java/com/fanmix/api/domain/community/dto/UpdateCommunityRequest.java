14bf5485b603f98486307be97db674e5fe1a1651
ujubongbong <ujubongbong@gmail.com>
2024-09-24 21:13:58 +0900

    :sparkles: feat: 커뮤니티 수정시 카테고리 추가

--- src/main/java/com/fanmix/api/domain/community/dto/UpdateCommunityRequest.java
+++ src/main/java/com/fanmix/api/domain/community/dto/UpdateCommunityRequest.java
@@ -1,5 +1,7 @@
 package com.fanmix.api.domain.community.dto;
 
+import com.fanmix.api.domain.community.entity.Category;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Getter
 public class UpdateCommunityRequest {
 	private int influencerId;
+	private Category category;
 	private String name;
 	private Boolean isShow;
 }
