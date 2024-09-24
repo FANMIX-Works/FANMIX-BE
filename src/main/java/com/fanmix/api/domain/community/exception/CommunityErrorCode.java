9fc2aebbedde68303335368a0adc7b3189e04e91
ujubongbong <ujubongbong@gmail.com>
2024-09-24 21:14:32 +0900

    :sparkles: feat: 카테고리, 권한 관련 예외 추가

--- src/main/java/com/fanmix/api/domain/community/exception/CommunityErrorCode.java
+++ src/main/java/com/fanmix/api/domain/community/exception/CommunityErrorCode.java
@@ -14,7 +14,9 @@ public enum CommunityErrorCode implements ErrorCode {
 	INFLUENCER_ID_DUPLICATION(HttpStatus.INTERNAL_SERVER_ERROR, "2-001", "이미 존재하는 팬채널입니다."),
 	NAME_DUPLICATION(HttpStatus.INTERNAL_SERVER_ERROR, "2-002", "이미 존재하는 커뮤니티입니다."),
 	COMMUNITY_NOT_EXIST(HttpStatus.INTERNAL_SERVER_ERROR, "2-003", "존재하지 않는 커뮤니티입니다."),
-	INVALID_FOLLOW_STATUS(HttpStatus.INTERNAL_SERVER_ERROR, "2-004", "유효하지 않은 상태입니다.");
+	INVALID_FOLLOW_STATUS(HttpStatus.INTERNAL_SERVER_ERROR, "2-004", "유효하지 않은 상태입니다."),
+	NOT_EXISTS_AUTHORIZATION(HttpStatus.UNAUTHORIZED, "2-005", "해당 작업을 수행할 권한이 없습니다."),
+	NOT_EXISTS_CATEGORY(HttpStatus.INTERNAL_SERVER_ERROR, "2-006", "존재하지 않는 카테고리입니다.");
 
 	private final HttpStatus httpStatus;
 	private final String customCode;
