e854e362604ce32ba1eee905fff8e6581ba1bd08
DESKTOP-ggoomter\ggoomter <ggoomter@gmail.com>
2024-09-27 18:19:22 +0900

    fix : 하드코딩한 테스트는 @Disabled 처리

--- src/test/java/com/fanmix/api/common/GoogleLoginServiceTest.java
+++ src/test/java/com/fanmix/api/common/GoogleLoginServiceTest.java
@@ -2,6 +2,7 @@ package com.fanmix.api.common;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +10,11 @@ import org.springframework.test.context.ActiveProfiles;
 
 import com.fanmix.api.domain.member.service.GoogleLoginService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootTest
 @ActiveProfiles("test")
+@Slf4j
 class GoogleLoginServiceTest {
 
 	@Autowired
@@ -24,14 +28,32 @@ class GoogleLoginServiceTest {
 	}
 
 	@Test
+	@Disabled
 	public void validateToken() {
+		//given
+
 		String tmp = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJnZ29vbXRlckBnbWFpbC5jb20iLCJpYXQiOjE3MjYyMzM2NzMsImV4cCI6MTcyNjMyMDA3M30.BymxpkUVxqySOc4rEmKPKaw55Sq8VYJdxI24NVI3Rk8";
-		googleLoginService.isValidateJwtToken(tmp);
+		boolean result = googleLoginService.isValidateJwtToken(tmp);
+
+		//when
+		log.debug("진짜 유효한지 검사 : " + result);
+
+		//then
+		assertTrue(result);
 	}
 
 	@Test
+	@Disabled
 	public void getAccessTokenUsingrefreshToken() {
+		//given
 		String refreshToken = "1//0etSGwBdL0HA-CgYIARAAGA4SNwF-L9Ireq4_ulFBX3OcLlZehTMLMhw2-camq1U7v8dJtXOXnzscao7FFFXjVwMY1gKdvICEk50";
-		googleLoginService.getNewAccessTokenUsingRefreshToken(refreshToken);
+
+		//when
+		String newAccessToken = googleLoginService.getNewAccessTokenUsingRefreshToken(refreshToken);
+
+		//then
+		log.debug("newAccessToken : " + newAccessToken);
+		assertNotNull(newAccessToken);
+
 	}
 }
