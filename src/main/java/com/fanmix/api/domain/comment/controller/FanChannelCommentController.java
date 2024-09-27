66de66a936273b7a3c76b51a5c9708f6375ad9c5
ujubongbong <ujubongbong@gmail.com>
2024-09-27 22:07:50 +0900

    :sparkles: feat: 팬채널 댓글 수정

--- src/main/java/com/fanmix/api/domain/comment/controller/FanChannelCommentController.java
+++ src/main/java/com/fanmix/api/domain/comment/controller/FanChannelCommentController.java
@@ -7,12 +7,14 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fanmix.api.common.response.Response;
 import com.fanmix.api.domain.comment.dto.AddCommentRequest;
 import com.fanmix.api.domain.comment.dto.CommentResponse;
+import com.fanmix.api.domain.comment.dto.UpdateCommentRequest;
 import com.fanmix.api.domain.comment.service.FanChannelCommentService;
 
 import lombok.RequiredArgsConstructor;
@@ -43,5 +45,16 @@ public class FanChannelCommentController {
 			.map(CommentResponse::new)
 			.toList();
 		return ResponseEntity.ok(Response.success(comments));
+	}
+
+	// 팬채널 댓글 수정
+	@PutMapping("/api/fanchannels/posts/{postId}/comments/{commentId}")
+	public ResponseEntity<Response<CommentResponse>> updateFanChannelComment(
+		@PathVariable int postId,
+		@PathVariable int commentId,
+		@RequestBody UpdateCommentRequest request,
+		@AuthenticationPrincipal String email) {
+		fanChannelCommentService.updateFanChannelComment(postId, commentId, request, email);
+		return ResponseEntity.ok(Response.success());
 	}
 }
--- src/main/java/com/fanmix/api/domain/comment/service/FanChannelCommentService.java
+++ src/main/java/com/fanmix/api/domain/comment/service/FanChannelCommentService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.fanmix.api.domain.comment.dto.AddCommentRequest;
+import com.fanmix.api.domain.comment.dto.UpdateCommentRequest;
 import com.fanmix.api.domain.comment.entity.Comment;
 import com.fanmix.api.domain.comment.exception.CommentErrorCode;
 import com.fanmix.api.domain.comment.exception.CommentException;
@@ -30,6 +31,7 @@ public class FanChannelCommentService {
 	private final MemberRepository memberRepository;
 
 	// 팬채널 댓글 작성
+	@Transactional
 	public Comment addFanChannelComment(int postId, AddCommentRequest request, String email) {
 		Post post = postRepository.findById(postId)
 			.orElseThrow(() -> new PostException(PostErrorCode.POST_NOT_EXIST));
@@ -66,5 +68,26 @@ public class FanChannelCommentService {
 			throw new CommentException(CommentErrorCode.NOT_EXISTS_AUTHORIZATION);
 		}
 		return post.getComments();
+	}
+
+	// 팬채널 댓글 수정
+	@Transactional
+	public Comment updateFanChannelComment(int postId, int commentId, UpdateCommentRequest request, String email) {
+		postRepository.findById(postId)
+			.orElseThrow(() -> new PostException(PostErrorCode.POST_NOT_EXIST));
+
+		Comment comment = commentRepository.findById(commentId)
+			.orElseThrow(() -> new CommentException(CommentErrorCode.COMMENT_NOT_EXIST));
+
+		Member member = memberRepository.findByEmail(email)
+				.orElseThrow(() -> new MemberException(MemberErrorCode.NO_USER_EXIST));
+
+		if(member.getRole().equals(Role.COMMUNITY)) {
+			throw new CommentException(CommentErrorCode.NOT_EXISTS_AUTHORIZATION);
+		}
+
+		comment.update(request.getIsDelete(), request.getContents());
+
+		return comment;
 	}
 }
