d2ef972f42c38faeb5bdb3d8eae58036bd556f11
Avocado <ujubongbong@gmail.com>
2024-10-02 00:24:53 +0900

    :bug: fix: 게시물 정렬 수정

--- src/main/java/com/fanmix/api/domain/post/repository/PostRepository.java
+++ src/main/java/com/fanmix/api/domain/post/repository/PostRepository.java
@@ -2,31 +2,24 @@ package com.fanmix.api.domain.post.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.fanmix.api.domain.post.entity.Post;
 
-import io.lettuce.core.dynamic.annotation.Param;
-
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
 	List<Post> findByCommunityId(int communityId);
-
-	@Query("SELECT COUNT(l) FROM PostLikeDislike l WHERE l.post.id = :postId")
-	int countPostLikeDislikesByPostId(@Param("postId") int postId);
 
 	@Query("SELECT p FROM Post p "
 		+ "JOIN FETCH p.community c "
 		+ "ORDER BY p.viewCount DESC")
 	List<Post> findTop5PopularPosts();
 
-	List<Post> findAllByCommunityIdOrderByLikeCountDesc(int communityId);
-	List<Post> findAllByCommunityIdOrderByViewCount(int communityId);
 	List<Post> findAllByCommunityIdOrderByCrDateDesc(int communityId);
 
 	List<Post> findAllByOrderByCrDateDesc();
-	List<Post> findAllByOrderByLikeCountDesc();
-	List<Post> findAllByOrderByViewCount();
+	List<Post> findAllByCommunityId(int communityId, Sort sort);
 }
