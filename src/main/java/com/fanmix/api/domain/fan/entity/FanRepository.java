3c91e4e383b9f76b24af3233533efa47b60e7340
Avocado <ujubongbong@gmail.com>
2024-10-03 21:07:08 +0900

    :bug: fix: fanRepository 중복 삭제

--- src/main/java/com/fanmix/api/domain/fan/entity/FanRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fanmix.api.domain.fan.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FanRepository extends JpaRepository<Fan, Long> {
-	long countByInfluencerIdAndFanStatus(Long influencerId, FanStatus fanStatus);
-}
