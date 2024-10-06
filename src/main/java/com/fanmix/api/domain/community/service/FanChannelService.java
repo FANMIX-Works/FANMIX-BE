f5beb382c04b7b195e0e560f9ee1e951590a9c39
Avocado <ujubongbong@gmail.com>
2024-10-07 00:15:09 +0900

    :sparkles: feat: 팬채널 리스트 정렬

--- src/main/java/com/fanmix/api/domain/community/service/FanChannelService.java
+++ src/main/java/com/fanmix/api/domain/community/service/FanChannelService.java
@@ -9,6 +9,7 @@ import com.fanmix.api.domain.community.exception.CommunityErrorCode;
 import com.fanmix.api.domain.community.exception.CommunityException;
 import com.fanmix.api.domain.community.repository.CommunityRepository;
 import com.fanmix.api.domain.fan.repository.FanRepository;
+import com.fanmix.api.domain.influencer.controller.InfluencerController;
 import com.fanmix.api.domain.influencer.entity.Influencer;
 import com.fanmix.api.domain.influencer.exception.InfluencerErrorCode;
 import com.fanmix.api.domain.influencer.exception.InfluencerException;
@@ -18,7 +19,6 @@ import com.fanmix.api.domain.member.exception.MemberErrorCode;
 import com.fanmix.api.domain.member.exception.MemberException;
 import com.fanmix.api.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Sort;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -60,28 +60,22 @@ public class FanChannelService {
 
 	// 팬채널 리스트 정렬
 	@Transactional(readOnly = true)
-	public List<FanChannelResponse> fanChannelList(String sort) {
-		Sort followerCountDesc = Sort.by(
-			Sort.Order.desc("influencer.followerCount"),
-			Sort.Order.desc("crDate")
-		);
-		Sort latestConfirmDate = Sort.by(
-			Sort.Order.desc("influencer.confirmDate"),
-			Sort.Order.desc("crDate")
-		);
-		Sort nameAsc = Sort.by(Sort.Order.asc("name"));
-
+	public List<FanChannelResponse> fanChannelList(String sort, String email) {
+		Member member = memberRepository.findByEmail(email)
+				.orElseThrow(() -> new MemberException(MemberErrorCode.NO_USER_EXIST));
 		List<Community> fanChannelList = switch (sort) {
-			// case "FOLLOWER_COUNT" -> communityRepository.findAllOrderByRegisteredFanCount();
-			case "LATEST_CHANNEL" -> communityRepository.findAll(latestConfirmDate);
-			default -> communityRepository.findAll(nameAsc);
+			case "FOLLOWER_COUNT" -> communityRepository.findAllByFollowerCountDesc();
+			case "LATEST_CHANNEL" -> communityRepository.findAllByAuthenticationConfirmDateDesc();
+			default -> communityRepository.findAllOrderByInfluencerName();
 		};
 
-		return fanChannelList
-			.stream()
-			.filter(fanchannel -> fanchannel.getInfluencer().getId() != null)
-			.map(FanChannelResponse::new)
-			.collect(Collectors.toList());
+		return fanChannelList.stream()
+				.filter(fanChannel -> fanChannel.getInfluencer() != null)
+				.map(community -> {
+					boolean isFan = fanRepository.existsByInfluencerAndMember(community.getInfluencer(), member);
+					return new FanChannelResponse(community, isFan);
+				})
+				.collect(Collectors.toList());
 	}
 
 	// 팬채널 정보 조회
@@ -93,8 +87,11 @@ public class FanChannelService {
 		if(!member.getRole().equals(Role.COMMUNITY)) {
 			throw new CommunityException(CommunityErrorCode.NOT_EXISTS_AUTHORIZATION);
 		}
-		return communityRepository.findById(communityId)
-			.orElseThrow(() -> new CommunityException(CommunityErrorCode.COMMUNITY_NOT_EXIST));
+		Community community = communityRepository.findById(communityId)
+				.orElseThrow(() -> new CommunityException(CommunityErrorCode.COMMUNITY_NOT_EXIST));
+
+		Boolean isFan = fanRepository.existsByInfluencerAndMember(community.getInfluencer(), member);
+		return new FanChannelResponse(community, isFan);
 	}
 
 	// 팬채널 수정
